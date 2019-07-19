4910a2e02c1262b9846cc42ddfe54548a77ef7ed
Kevin Zhou <kevinsoft@126.com>
2019-07-19 21:54:59 +0800

    Array Sort and Print
    java.util.Arrays.sort

--- Test/src/ch4/kevin/com/ArrayRefDemo01.java
+++ Test/src/ch4/kevin/com/ArrayRefDemo01.java
@@ -8,10 +8,18 @@ public class ArrayRefDemo01 {
 
 	public static void main(String[] args) {
          int [] temp = { 1, 3, 5 } ;
-         temp[0] = 6 ;
-         for ( int i = 0; i < temp.length; i++ ) {   //循环输出数组
+         
+         fun(temp) ;
+         for ( int i = 0; i < temp.length; i++ ) {   //循环输出数组，查看数组内容
         	 System.out.println(temp[i]);
          }
-	}
+         
+      
+	 }
+	   public static void fun( int x[]) {
+      	 x[0] = 6 ;    //修改数组第一个元素的内容
+      	 
+       }
 
 }
+
