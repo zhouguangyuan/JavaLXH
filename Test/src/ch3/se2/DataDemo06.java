1f1b46ae30b65458937c9824a8600ad436e5f9bc
zhouguangyuan <kevinsoft@126.com>
2020-04-06 14:13:24 +0800

    repair DataDemo06
    add annotations

--- Test/src/ch3/se2/DataDemo06.java
+++ Test/src/ch3/se2/DataDemo06.java
@@ -3,13 +3,20 @@ package ch3.se2;
  * @author 作者 Kevin Zhou 
  * @version 创建时间：2020年4月4日 下午7:17:35 
  * 类说明 
- * 使用浮点型数值时，默认的类型是double.在数值后面可加上D或者d,作为
- * double类型的标识。因此在Java中，D或d是可有可无的。
+ *一、使用浮点型数值时，默认的类型是double.
+ * 在数值后面可加上D或者d,作为 double类型
+ * 的标识。在Java中，D或d是可有可无的。
+ * 二、在数据后面加上F或f,则作为float类型识别。
+ * 若没有加上，Java就会将数据默认作为double类
+ * 型，而此时与声明类型float不匹配，编译时会
+ * 出错，提示可能会丢失精度。
+ * 
  */
 public class DataDemo06 {
 	public static void main(String []arg){
 		float num = 3.0f; //定义float型变量
-		System.out.println("两个数相乘," + num * num ); //计算两数相乖
+		//float num2 = 3.0; //知识点二，
+		System.out.println("两个数相乘," + num * num ); //计算两数相乖，知识要点一
 	}
 
 }
