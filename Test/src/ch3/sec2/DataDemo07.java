4d5abab506100e89d706379e3f8fdf5e13e3f3e9
zhouguangyuan <kevinsoft@126.com>
2020-04-27 11:59:00 +0800

    Conditionan Sentence
    if
    If else
    If else if else
    Switch

---
+++ Test/src/ch3/sec2/DataDemo07.java
@@ -0,0 +1,20 @@
+package ch3.sec2;
+/** 
+ * @author 作者 Kevin Zhou 
+ * @version 创建时间：2020年4月12日 下午2:14:16 
+ * 类说明 
+ * 知识点：布尔(boolean)类型的变量只有true（真）
+ * 和false（假）两种取值。
+ * 布尔类型变量默认值为false.
+ * 布尔值，通常用来控制程序的流程。
+ */
+public class DataDemo07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean flag = true; //声明布尔变量flag,并赋值为true
+		System.out.println("flag =" + flag);
+
+	}
+
+}
---
+++ Test/src/ch3/sec5/IfDemo.java
@@ -0,0 +1,19 @@
+package ch3.sec5;
+
+/**
+ * @author 作者 Kevin Zhou
+ * @version 创建时间：2020年4月24日 下午7:25:39 
+ * 类说明：If循环语句
+ */
+public class IfDemo {
+	public static void main(String[] args) {
+		int x = 3;
+		int y = 10;
+
+		if (x > y) {
+			System.out.println("x大于y");
+		}
+		if (x < y)
+			System.out.println("x小于y");
+	}
+}
