3132bee9748c0dca9f1ccf0784c376224172c99e
zhouguangyuan <kevinsoft@126.com>
2020-05-06 11:58:10 +0800

    The Arrays
    Update the Arrays

--- Test/src/ch4/kevin/com/ArrayDemo02.java
+++ Test/src/ch4/kevin/com/ArrayDemo02.java
@@ -1,23 +1,25 @@
 package ch4.kevin.com;
 
 public class ArrayDemo02 {
-
+/*
+数组动态初始化：采用先声明数组，然后再为数组中的每个内容赋值的方式。
+*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		int score [] = null;
-		
+
 		score = new int[3];
-		
+
 		System.out.println("score[0] = "+ score[0]);
 		System.out.println("score[1] = "+ score[1]);
 		System.out.println("score[2] = "+ score[2]);
-		
+
 		for( int x = 0; x <= 2; x++)
 		{
 			score[x] = x*2 + 1;
 		}
-		
+
 		for( int x = 0; x <= 2; x++)
 		{
 			System.out.println("score[" + x + "] = " + score[x]);
