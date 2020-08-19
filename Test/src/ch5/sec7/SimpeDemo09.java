bea4b3649832d5683f873ea287dd20ef52951a4d
zhouguangyuan <kevinsoft@126.com>
2020-08-19 15:59:25 +0800

    String Class
    Update  the String Demo

---
+++ Test/src/ch5/sec7/SimpeDemo09.java
@@ -0,0 +1,20 @@
+package ch5.sec7;
+
+/**
+ * @author Kelvin
+ * @version 2020年8月17日 下午3:48:37
+ * @class instruction
+ */
+public class SimpeDemo09 {
+
+	public static void main(String[] args) {
+		String str = "Lixinghua";
+
+		for (int i = 0; i < 100; i++) {
+			str += i;
+		}
+		
+		System.out.println(str);
+
+	}
+}
