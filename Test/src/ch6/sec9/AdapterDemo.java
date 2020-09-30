9ac97a3b01a4cb56c0ca454356328503fd391e35
zhouguangyuan <kevinsoft@126.com>
2020-09-30 17:48:55 +0800

    Create the AdapterDemo class

---
+++ Test/src/ch6/sec9/AdapterDemo.java
@@ -0,0 +1,49 @@
+package ch6.sec9;
+
+interface Window {
+	public void open();
+
+	public void close();
+
+	public void activated();
+
+	public void iconified();
+
+	public void deiconified();
+}
+
+abstract class WindowAdapter implements Window {
+	public void activated() {
+	}
+
+	public void close() {
+	}
+
+	public void iconified() {
+	}
+
+	public void deiconified() {
+	}
+
+	public void open() {
+	}
+}
+
+class WindowImpl extends WindowAdapter {
+	public void open() {
+		System.out.println("窗口打开。");
+	}
+
+	public void close() {
+		System.out.println("窗口关闭。");
+	}
+}
+
+public class AdapterDemo {
+	public static void main(String[] args) {
+		Window win = new WindowImpl();
+		win.open();
+		win.close();
+	}
+
+}
